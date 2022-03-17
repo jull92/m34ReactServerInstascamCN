e34bc64166382e814ffe54221f8cde80ddbf8bc7
jull92 <julia.kata92@gmail.com>
2022-03-17 11:57:33 +0000

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
+
+import {useState, useEffect} from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import {Login} from "./components/login"
+import {Home} from "./components/home"
+import { tokenLogin } from "./utils";
 import './App.css';
 
-function App() {
+const App= () => {
+  const [user, setUser] = useState();
+  // what useState looks like
+  // const useState = (initialVal) => {
+  //   let state = initialVal;
+  //   const setState = (newVal) => {
+  //     state = newVal
+  //   }
+  // };
+  // return [state, setState];
+  useEffect(() => {
+    if (user) {
+      localStorage.setItem("myToken", user);
+      tokenLogin(setUser);
+    };
+    // console.log(user.token);
+    // localStorage.setItem("myToken", user.token);
+    // tokenLogin(setUser);
+  }, [user]);
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Login setUser={setUser} user={user} />} />
+        <Route path="/home" element={<Home user={user} setUser={setUser} />} />
+      </Routes>
+    </BrowserRouter>
   );
-}
+};
+
 
 export default App;
---
+++ src/components/photoContainer/index.js
@@ -0,0 +1,10 @@
+import './index.css'
+
+export const PhotoContainer = ({photo}) => {
+    return (
+        <>
+            <p>{photo.author}</p>
+            <img src={photo.download_url} alt="lorem picsum random" class="photo"/>
+        </>
+    )
+};
